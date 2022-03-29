d28057204cc5d1febc443ba012d5e087591a1821
shiyunkhoo <shiyun.khoo@gmail.com>
2022-03-29 10:45:20 +0800

    TheLEAD Day 1 code 28032022

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <p class="mt-5"><img src="https://tinyurl.com/359282x2"></p>
     </div>
+    <div class="m-auto">
+      <h1 class="text-5xl">Green Homes</h1>
+      <p class="mt-5">Affordable Solutions for Environmentally Friendly Abodes</p>
+      <p class="mt-5"><strong>Navigation Bar</strong></p>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Products</li>
+          <li>Meet Our Team</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+    </div>
+    <div class="m-auto">
+      <h2><strong>Welcome Video</strong></h2>
+      <p class="mt-5"> embed video
+      </p>
+      <p class="mt-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <p> Excepteur sint occaecat cupidatat, ut enim ad minim veniam.</p>
+    </div>
+    <div class="m-auto">
+      <h2><strong>Products Overview</strong></h2>
+      <p>(table)</p>
+    </div>
+    <footer>
+      <p> Contact </p>
+      <p> Email </p>
+      <p> Phone </p>
+        <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+    </footer>
   </div>
 </template>
