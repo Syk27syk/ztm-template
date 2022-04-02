ca5b5fbf85f00c19543e95e374b92b52980d40f8
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-02 18:20:40 +0800

    anmiate 2nd april 2022

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
         <div id="menu" class="grid grid-cols-2 py-5">
           <div id="menu-left">
-            <img src="logo.png" class="w-[150px]" alt="" />
+            <img src="logo.png" class="w-[150px] animate-bounce" alt="" />
           </div>
           <div id="menu-right">
             <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
@@ -17,8 +17,8 @@
           </div>
         </div>
         <div class="grid grid-cols-2 pb-20 pt-10">
-          <img src="hero-burger.png" class="pr-8 mt-10" alt="" />
-          <div>
+          <img src="hero-burger.png" class="pr-8 mt-10 animate-wiggle" alt="" />
+          <div class="md:animate-spin lg:animate-none">
             <h1 class="text-8xl text-white text-center font-lilita uppercase">
               Classic Burger
             </h1>
@@ -38,7 +38,7 @@
         <p class="font-roboto font-light text-gray-500 text-lg py-6 px-28 text-center">
             Spend quality time with your family and eat an affordable, nutritious and delicious meal together. Not all burgers are fast foods. All our burgers are handmade to order for you and your family members to enjoy.
         </p>
-        <button class="font-oswald uppercase bg-red-500 text-white text-center py-3 px-8">
+        <button class="font-oswald uppercase bg-red-500 text-white text-center py-3 px-8 animate-pulse">
           <a href="">Explore Full Menu</a>
         </button>
       </div>
