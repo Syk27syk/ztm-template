d5cc5d8837d01327c8303adf35bfbee998f497f2
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-09 06:46:26 +0800

    makerspaceJB home

---
+++ pages/MakerspaceJB/home.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div id="header">
+            <div id="nav-bar">
+                <div id="logo">
+                    <img src="" alt="">
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Calendar</a></li>
+                        <li><a href="">Location</a></li>
+                        <li><a href="">Membership</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Connect</a></li>
+                        <li><a href="">Docs</a></li>
+            </div>
+            <div id="hero-banner class="m-16"">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div id="body" class="m-24">
+            <div id="opening-message" class="py-5">
+                <h1 class="py-2">Visiting the space during COVID-19</h1>
+                <p class="text-sm">last updated: 19 March 2022<p>
+                <p class="py-2">HackerspaceSG is open for members to work on their own projects.</p>
+                <p class="py-2">If you'd like to visit the space or <a href="">join as a member</a>, email us with your preferred times at <a href="">contact@hackerspace.sg</a>, or ask if someone can host you on our <a href="">Telegram gruop chat.</a></p>
+            </div>
+        <div id="upcoming events">
+            <div>
+                <Plenum />
+            </div>
+        </div>
+        <div id="tweets">
+            <p> embed twitter timeline </p>
+        </div>
+        <div id="information" class="grid grid-cols-3">
+            <div id="contribute">
+                <ul>
+                    <li><a href="">Open Collective</a></li>
+                    <li><a href="">GitHub Sponsors</a></li>
+                </ul>
+            <div id="Interact">
+                <ul>
+                    <li><a href="">Telegram Chat</a></li>
+                    <li><a href="">Google Group</a></li>
+                    <li><a href="">Facebook Group</a></li>
+                    <li><a href="">GitHub</a></li>
+                </ul>
+            <div id="Follow">
+                <ul>
+                    <li><a href="">Facebook</a></li>
+                    <li><a href="">Twitter</a></li>
+                    <li>... and more!</li>
+                </ul>
+        </div>
+        <div id="footer">
+            <p>Copyright (C) 2009-2022 Hackerspace.SG Pte Ltd and contributors.</p>
+        </div>
+    </div>
+</template>
