b9098869d24b4cb65cccc85998f230738736c19b
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-09 07:03:42 +0800

    makerspaceJB home page html

--- pages/MakerspaceJB/home.vue
+++ pages/MakerspaceJB/home.vue
@@ -1,61 +1,74 @@
 <template>
     <div>
-        <div id="header">
-            <div id="nav-bar">
+        <div id="head">
+            <div id="nav-bar flex flex-row">
                 <div id="logo">
-                    <img src="" alt="">
+                    <img src="" alt="" class="w-32 h-32">
                 </div>
-                <div>
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Calendar</a></li>
-                        <li><a href="">Location</a></li>
-                        <li><a href="">Membership</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">FAQ</a></li>
-                        <li><a href="">Connect</a></li>
-                        <li><a href="">Docs</a></li>
+                <div class="flex flex-row bg-black text-white text-xl text-semibold px-24">
+                    <span class="px-5 py-2"><a href="">Home</a></span>
+                    <span class="px-5 py-2"><a href="">Calendar</a></span>
+                    <span class="px-5 py-2"><a href="">Location</a></span>
+                    <span class="px-5 py-2"><a href="">Membership</a></span>
+                    <span class="px-5 py-2"><a href="">About</a></span>
+                    <span class="px-5 py-2"><a href="">FAQ</a></span>
+                    <span class="px-5 py-2"><a href="">Connect</a></span>
+                    <span class="px-5 py-2"><a href="">Docs</a></span>
+                </div>
             </div>
-            <div id="hero-banner class="m-16"">
+            <div id="hero-banner" class="m-32">
                 <img src="" alt="">
+                <h1 class="text-oswald text-6xl">MakerspaceJB</h1>
             </div>
         </div>
         <div id="body" class="m-24">
             <div id="opening-message" class="py-5">
-                <h1 class="py-2">Visiting the space during COVID-19</h1>
-                <p class="text-sm">last updated: 19 March 2022<p>
-                <p class="py-2">HackerspaceSG is open for members to work on their own projects.</p>
-                <p class="py-2">If you'd like to visit the space or <a href="">join as a member</a>, email us with your preferred times at <a href="">contact@hackerspace.sg</a>, or ask if someone can host you on our <a href="">Telegram gruop chat.</a></p>
+                <h3 class="text-oswald text-3xl py-2">Visiting the space during COVID-19</h3>
+                <p class="text-sm">last updated: 19 March 2022</p>
+                <p class="py-2">MakerspaceJB is open for members to work on their own projects.</p>
+                <p class="py-2">If you'd like to visit the space or <a href="">join as a member</a>, email us with your preferred times at <a href="">contact@mackerspaceJB.my</a>, or ask if someone can host you on our <a href="">Telegram gruop chat.</a></p>
             </div>
-        <div id="upcoming events">
-            <div>
-                <Plenum />
+            <div id="upcoming events">
+                <h3 class="text-3xl"> Upcoming Events </h3>
+                <div>
+                    <Plenum />
+                    <Plenum />
+                    <Plenum />
+                    <Plenum />
+                </div>
+            </div>
+            <div id="tweets">
+                <h3 class="text-3xl">Tweets</h3>
+                <p>(embed twitter timeline)</p>
             </div>
         </div>
-        <div id="tweets">
-            <p> embed twitter timeline </p>
-        </div>
-        <div id="information" class="grid grid-cols-3">
+        <div id="information" class="grid grid-cols-3 p-32 bg-black text-white text-center">
             <div id="contribute">
+                <h4 class="text-xl text-oswald text-bold">Contribute</h4>
                 <ul>
                     <li><a href="">Open Collective</a></li>
                     <li><a href="">GitHub Sponsors</a></li>
                 </ul>
+            </div>
             <div id="Interact">
+                <h4 class="text-xl text-oswald text-bold">Interact</h4>
                 <ul>
                     <li><a href="">Telegram Chat</a></li>
                     <li><a href="">Google Group</a></li>
                     <li><a href="">Facebook Group</a></li>
                     <li><a href="">GitHub</a></li>
                 </ul>
+            </div>
             <div id="Follow">
+                <h4 class="text-xl text-oswald text-bold">Follow</h4>
                 <ul>
                     <li><a href="">Facebook</a></li>
                     <li><a href="">Twitter</a></li>
                     <li>... and more!</li>
                 </ul>
+            </div>
         </div>
-        <div id="footer">
+        <div id="footer" class="bg-black text-white text-center py-5 px-32">
             <p>Copyright (C) 2009-2022 Hackerspace.SG Pte Ltd and contributors.</p>
         </div>
     </div>
