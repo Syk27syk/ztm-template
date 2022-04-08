84e72fe5261b447d62a9f154b93851749937c10a
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-08 18:04:50 +0800

    day 10 assignment

--- store/index.js
+++ store/index.js
@@ -1,13 +1,26 @@
 const state = () => ({
-    orders:[]
+  orders: []
 })
 
 const mutations = {
-    addItem(state, selectedItem){
-        state.orders.push({
-            name: selectedItem.name,
-            price: selectedItem.price,
-            quantity: 1,
-        })
+  addItem(state, selectedItem) {
+    const index = state.orders.findIndex((item) => {
+      return item.name === selectedItem.name;
+    });
+
+    if (index === -1) {
+      state.orders.push({
+        name: selectedItem.name,
+        price: selectedItem.price,
+        quantity: 1,
+      });
+    } else {
+      state.orders[index].quantity++;
     }
-}
+  },
+};
+
+export default {
+  state,
+  mutations,
+};
