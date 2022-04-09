7e0a19238e0f71bb676e906817379ec524751fe2
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-09 22:48:13 +0800

    portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,12 +1,168 @@
 <template>
     <div>
-        <h1> list of sites </h1>
-        <div>
-            <ul>
-                <li><a href="/burger/index.vue">Testo Burgers</a></li>
-                <li><a href="/millinery/index.vue">Testo Burgers</a></li>
-                <li><a href="/greenhomes/index.vue">Testo Burgers</a></li>
-            </ul>
+        <div id="websites">
+            <h1>Websites</h1>
+            <p> sort by: website type | website design | add tags </p>
+            <div id="communities">
+                <h4 class="text-3xl">
+                    Communities
+                </h4>
+                <ul>
+                    <li><a href="/makerspacejb/home">Makerspace JB</a></li>
+                    <li><a href="/pollinatorentrecamp/home">Pollinator Entrecamp</a></li>
+                    <li><a href="/kettlesyard/home">KettlesYard</a></li>
+                </ul>
+            </div>
+            <div id="smes">
+                <h4 class="text-3xl">
+                    Small and Medium Business Entreprises
+                </h4>
+                <ul>
+                    <li><a href="/burger/index">Testo Burgers</a></li>
+                    <li><a href="/hirosushi">Hirosushi</a></li>
+                    <li><a href="/millinery/index">Luciano's Millinery</a></li>
+                </ul>
+            </div>
+            <div id="portfolios">
+                <h4 class="text-3xl">
+                    Portfolios
+                </h4>
+                <ul>
+                    <li><a href="/pixionstudios">PixionStudios</a></li>
+                    <li><a href="/purityofnoise">PurityofNoise</a></li>
+                </ul>
+            </div>
+            <div id="corporations">
+                <h4 class="text-3xl">
+                    National and Multinational Corporations
+                </h4>
+                <ul>
+                    <li><a href="/pathfinder">Pathfinder Consulting</a></li>
+                    <li><a href="/pierreherme">Pierre Herme</a></li>
+                    <li><a href="/sonoforte">Sonoforte</a></li>
+                    <li><a href="/greenhomes">Green Homes International</a></li>
+                    <li><a href="/element14clone">Element14Clone</a></li>
+                    <li><a href="/ventusairlines">Ventus Airlines</a></li>
+                </ul>
+            </div>
+            <div id="ecomm">
+                <h4 class="text-3xl">
+                    E-commerce Stores
+                </h4>
+                <ul>
+                    <li><a href="">SecondHandEverything</a></li>
+                    <li><a href="">NookLook</a></li>
+                    <li><a href="">HappyBunch</a></li>
+                </ul>
+            </div>
+            <div id="booking">
+                <h4 class="text-3xl">
+                    Booking and Rating Platforms
+                </h4>
+                <ul>
+                    <li>BookingPro</li>
+                </ul>
+            </div>
+            <div id="trailers">
+                <h4 clas="text-3xl">
+                    Movie and Book trailers
+                </h4>
+                <ul>
+                    <li><a href="">Suspiria</a></li>
+                </ul>
+            </div>
+            <div id="gov">
+                <h4 class="text-3xl">
+                    Government Departments
+                </h4>
+                <ul>
+                    <li><a href="">Ministry of Health</a></li>
+                    <li><a href="">Department of Transport</a></li>
+                    <li><a href="">Tourism Asia</a></li>
+                    <li><a href="">Education Department</a></li>
+                </ul>
+            </div>
+            <div id="fun">
+                <h4 class="text-3xl">
+                    Fun and Curiosity
+                </h4>
+                <ul>
+                    <li>Random (Names,Websites,Art,Music,Books)</li>
+                    <li>Wineries in the World</li>
+                    <li>Coffee Producers</li>
+                    <li>Castles</li>
+                    <li>3D Tour of the House of Maurice Ravel</li>
+                </ul>
+            </div>
+        </div>
+        <div id="web-applications">
+            <div id="learn">
+                <h4 class="text-3xl">
+                    Learn
+                </h4>
+                <ul>
+                    <li>WebDev Tutor</li>
+                    <li>Statistics Tutor</li>
+                </ul>
+            </div>
+            <div id="analyze">
+                <h4 class="text-3xl">
+                    Analyze
+                </h4>
+                <ul>
+                    <li>Easy BoE</li>
+                    <li>Rapid Investment Analyzer</li>
+                    <li>Sound Analyzer</li>
+                </ul>
+            </div>
+            <div id="shop">
+                <h4 class="text-3xl">
+                    Shop
+                </h4>
+                <ul>
+                    <li>Second Hand Everything</li>
+                </ul>
+            </div>
+            <div id="search">
+                <h4 class="text-3xl">
+                    Search
+                </h4>
+                <ul>
+                    <li>FindYourHealthcareProvider</li>
+                    <li>FindYourCarMechanic</li>
+                </ul>
+            </div>
+            <div id="care">
+                <h4 class="text-3xl">
+                    Care
+                </h4>
+                <ul>
+                    <li>CatApp</li>
+                </ul>
+            </div>
+            <div id="create">
+                <h4 class="text-3xl">
+                    Create
+                </h4>
+                <ul>
+                    <li>Write a Song</li>
+                    <li>Write a Book</li>
+                </ul>
+            </div>
+        </div>
+        <div id="information">
+            <div>
+                <h4>Contact</h4>
+            </div>
+            <div>
+                <h4>For Clients</h4>
+            </div>
+            <div>
+                <h4>Disclaimer</h4>
+            </div>
+        </div>
+        <div id="footer">
+            @ 2022 All Rights Reserved | <a href="">About</a> | <a href="">Terms Of Use</a> | <a href="">Privacy Policy</a>
         </div>
     </div>
 </template>
