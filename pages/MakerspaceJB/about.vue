e1839567aae43f6335e5d6151404a612fcf16f98
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-09 10:27:07 +0800

    makerspacejb

--- pages/MakerspaceJB/about.vue
+++ pages/MakerspaceJB/about.vue
@@ -13,13 +13,42 @@
         </div>
         <div>
             <div id="about">
-                <p> about </p>
+                <h4 class="text-4xl text-black text-oswald">
+                    About
+                </h4>
+                <p>MakerspaceJB is Johor Bahru's very own Makerspace</p>
+                <p>There are hundreds of such <a href="" class="text-blue-400">hackerspaces</a> around the world, where people with common interests in computers, technology, , science, digital and electronic art, come together to meet, socialise and collaborate.</p>
+                <p>Built by and for geeks, nerds, inventors, engineers, and entrepreneurs, MakerspaceJB is the Johorean hacker community's home, living room and laboratory. Come hang out with the community in the evenings. Co-work with us during the day. We also host regular events, meetups, presentations, workshops, and movie nights for the local community.</p>
+                <p>MakerspaceJB was JB's first co-working space. Since we launched, many more have appeared - see <a href="" class="text-blue-400">list of co-working spaces in Johor</a>.</p>
             </div>
             <div id="facilities">
-                <p> facilities </p>
+                <h4 class="text-4xl text-black text-oswald">
+                    Facilities
+                </h4>
+                <ul>
+                    <li>Makerbot <a href="" class="text-blue-400">Cupcake CNC 3D printer</a></li>
+                    <li>Flashforge Creator 3D printer</li>
+                    <li><a href="" class="text-blue-400">RIPE Atlas Probe</a></li>
+                    <li><a href="" class="text-blue-400">Kitchen - fridge, freezer, dishwasher, microwave and toaster ovens</a></li>
+                    <li><a href="" class="text-blue-400">Shower</a></li>
+                    <li><a href="" class="text-blue-400">Development boards bank</a></li>
+                </ul>
             </div>
             <div id="pressmentions">
-                <p> press mentions </p>
+                <h4 class="text-4xl text-black text-oswald">
+                    Press Mentions
+                </h4>
+            </div>
+            <div id="acknowledgements">
+                <p>
+                    Members of the local hacker community have selflessly volunteered their time, energy, and money to make this happen.
+                </p>
+                <p>
+                    The members of MakerspaceJB acknowledge and appreciate the efforts of the community in helping put the space together. The following list is not exhaustive.
+                </p>
+                <p>
+                    Reuben Ch'ng, Lau Cher Han, Koh Wyhow, Seng Yung Quek, MK Chan, Wise Lee, Nazura, CJ Phua, Carol Tan, Sri Barratan, Teoh Alvin, Azhar Ramadhan, Martin Makial, Alex Foong, Ash.
+                </p>
             </div>
         </div>
         <div id="bottom">
