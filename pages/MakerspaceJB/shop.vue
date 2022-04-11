b967cb9a1df6c3d2e4624637c1ba3f282e7027e5
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-12 04:48:28 +0800

    makerspace shop

--- pages/MakerspaceJB/shop.vue
+++ pages/MakerspaceJB/shop.vue
@@ -1,23 +1,20 @@
 <template>
     <div>
-        <div id="header">
-            <p>header</p>
+        <div id="head">
+            <div>
+                <MakerspaceNavBar />
+            </div>
+            <div id="hero-banner" class="p-32 bg-[url('/makerclass/robot-shopping.jpg')] bg-center bg-cover min-h-96">
+                <h1 class="text-white text-6xl font-oswald uppercase text-center">
+                    workshops
+                </h1>
+            </div>
         </div>
-        <div id="body">
-            <div id="merchandise">
-                <h3 class="text-3xl text-black text-oswald uppercase">
-                    merchandise
-                </h3>
-                <div id="items">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-            </div>
-            <div id="gifts">
-                <h3 class="text-3xl text-black text-oswald uppercase">
-                    gifts
-                </h3>
+        <div id="gifts">
+            <h3 class="text-3xl text-black text-oswald uppercase bg-blue-300 py-3 text-center mx-auto">
+                Gifts
+            </h3>
+            <div class="grid grid-cols-3 gap-5 my-10 mx-24">
                 <div id="items">
                     <Product
                     />
@@ -27,11 +24,47 @@
                     />
                 </div>
             </div>
-            <div id="makertools">
-                <p>makertools</p>
+        </div>
+        <div id="Merchandise">
+            <h3 class="text-3xl text-black text-oswald uppercase bg-blue-300 py-3 text-center mx-auto">
+                Merchandise
+            </h3>
+            <div class="grid grid-cols-3 gap-5 my-10 mx-24">
+                <div id="items">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
             </div>
-            <div id="courses">
-                <p>courses</p>
+        </div>
+        <div id="makertools">
+            <h3 class="text-3xl text-black text-oswald uppercase bg-blue-300 py-3 text-center mx-auto">
+                Makertools
+            </h3>
+            <div class="grid grid-cols-3 gap-5 my-10 mx-24">
+                <div id="items">
+                    <Product
+                    />
+                    <Product
+                    />
+                    <Product
+                    />
+                </div>
+            </div>
+        </div>
+        <div id="makertools">
+            <h3 class="text-3xl text-black text-oswald uppercase bg-blue-300 py-3 text-center mx-auto">
+                Makertools
+            </h3>
+            <div class="grid grid-cols-3 gap-5 my-10 mx-24">
+                <div id="items">
+                    <Product
+                    />
+                    <Product
+                    />
+                    <Product
+                    />
+                </div>
             </div>
         </div>
     </div>
