858a3a12cf5fba371bc928d5efab9601874b971d
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-12 08:09:16 +0800

    non functional cart

---
+++ pages/MakerspaceJB/cart.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div id="head">
+            <div>
+                <MakerspaceNavBar />
+            </div>
+            <div id="hero-banner" class="px-32 py-36 bg-[url('/makerclass/robot-shopping.jpg')] bg-center bg-cover h-full">
+                <h1 class="text-white text-8xl font-oswald uppercase text-center">
+                    shop
+                </h1>
+            </div>
+        </div>
+        <div>
+           <table class="table-auto w-2/3 mt-20 mx-auto">
+                <thead class="text-3xl font-oswald text-black uppercase">
+                    <tr>
+                        <th class="text-left">Name</th>
+                        <th class="text-center">Price</th>
+                        <th class="text-center">Quantity</th>
+                        <th class="text-center">Total</th>
+                        <th class="text-center">Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="product in $store.state.orders"
+                        :key="product.name"
+                        class="font-oswald uppercase text-2xl border-b"
+                    >
+                            <td class="py-10">{{ product.name }}</td>
+                            <td class="text-right">RM {{ product.price }}</td>
+                            <td class="text-center">{{ order.quantity }}</td>
+                            <td class="text-right">RM {{ (order.price*order.quantity) }}</td>
+                            <td class="text-center"><button @click="removeItem">✖️</button></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left text-3xl text-black font-black text-oswald uppercase">Grand Total</td>
+                        <td class="invisible">price</td>
+                        <td class="text-right text-3xl text-black font-black text-oswald uppercase">quantity</td>
+                        <td class="text-right text-3xl text-black font-black text-oswald uppercase">sum</td>
+                        <td class="invisible">remove</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button class="bg-red-500 text-white text-oswald text-2xl text-center px-8 py-3 rounded my-12 grid grid-cols-1 item-center">
+                <NuxtLink to="/burger/checkout">Proceed to Checkout</NuxtLink>
+            </button>
+        </div>
+    </div>
+</template>
