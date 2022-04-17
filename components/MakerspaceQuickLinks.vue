808112b925f313008b11660baa1de3069ef263be
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-17 21:13:40 +0800

    quicklinks dropdown part 3

--- components/MakerspaceQuickLinks.vue
+++ components/MakerspaceQuickLinks.vue
@@ -2,7 +2,7 @@
     <div class="bg-white bg-opacity-20 rounded-full border border-gray-500 text-white p-5 absolute right-0 top-0 m-5">
         <button
             type="button"
-            class="text-xl font-extrabold font-roboto text-gray-500 py-2 text-center flex flex-col"
+            class="text-xl font-extrabold font-roboto text-gray-500 py-2 text-center flex flex-col relative z-10"
             @click="isOpen = !isOpen"
         >
             Quick<br>Links
@@ -16,7 +16,8 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
         </button>
-        <div v-if="isOpen" class="flex flex-col">
+        <button v-if="isOpen" class="fixed top-0 right-0 bottom-0 left-0 h-full w-full text-transparent cursor-default tabindex=-1 z-0" @click="isOpen = false">text</button>
+        <div v-if="isOpen" class="flex flex-col relative z-10">
             <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
                 <NuxtLink to="/makerspacejb/cart"><img src="/makerspaceJB/cart.png" alt="account"></NuxtLink>
             </button>
