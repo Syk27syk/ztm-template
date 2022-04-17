bc50fa9b0bf688a6c84ce7fdd4bcfdb824fed0a2
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-17 20:08:35 +0800

    hover zoom

--- components/MakerspaceProduct.vue
+++ components/MakerspaceProduct.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div id="" class="flex flex-col border border-gray-500 rounded-md p-5 bg-white hover:ring-8 hover:ring-yellow-200 relative">
-            <img :src="image" alt="galileo-thermometer" class="w-full">
+            <img :src="image" alt="" class="w-full hover:scale-150 hover:bg-white hover:border z-50">
             <h4 class="text-2xl text-black font-black font-oswald uppercase text-center py-3 h-20">{{ name }}</h4>
             <p id="description" class="text-oswald text-xl h-48">
                 {{ description }}
--- pages/MakerspaceJB/shop.vue
+++ pages/MakerspaceJB/shop.vue
@@ -16,21 +16,21 @@
             </h3>
             <div id="products" class="grid grid-cols-3 gap-5 pb-32 pt-10 mx-24">
                 <MakerspaceProduct
-                    image="/makerclass/galileo-thermometer.png"
+                    image="/makerspacejb/galileo-thermometer.png"
                     name="Galileo Thermometer"
                     description="Made of a sealed glass cylinder containing a clear liquid and several glass vessels of varying density, the individual floats rise or fall due to density changes as the temperature changes."
                     price="49.99"
                     link="/makerspaceJB/galileoThermometer"
                 />
                 <MakerspaceProduct
-                    image="/makerclass/beetle-bot.jpeg"
+                    image="/makerspacejb/beetle-bot.png"
                     name="Beetle Bot"
                     description="Robotic beetle that moves in response to touch and sound. Comes in various colors, for ages 5 and up."
                     price="14.85"
                     link="/makerspaceJB/beetleBot"
                 />
                  <MakerspaceProduct
-                    image="/makerclass/crookes-radiometer.png"
+                    image="/makerspacejb/crookes-radiometer.png"
                     name="Crookes Radiometer"
                     description="Invented in 1873 by chemist Sir William Crookes, this device rotates faster when exposed to light, providing a quantitative measurement of electromagnetic radiation intensity."
                     price="319.75"
