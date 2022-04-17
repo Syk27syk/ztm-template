6a3021415464f36f8f102d8bbaa58be2db583a97
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-17 17:24:48 +0800

    makerspaceJB quick access

--- components/MakerspaceQuickAccess.vue
+++ components/MakerspaceQuickAccess.vue
@@ -1,21 +1,18 @@
 <template>
-    <div class="bg-blue-500 bg-opacity-700 text-white p-5 absolute right-0 top-0">
-        <h3 class="text-xl">Quick Links</h3>
+    <div class="bg-white bg-opacity-20 rounded-full border border-gray-500 text-white p-5 absolute right-0 top-0 m-5">
+        <h3 class="text-xl font-extrabold font-roboto text-gray-500 py-2">Quick<br>Links</h3>
         <div class="flex flex-col">
-            <button class="rounded-full bg-yellow-300">
-                <img src="/makerclass/cart.png" alt="account">
+            <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                <NuxtLink to="/makerspacejb/cart"><img src="/makerspaceJB/cart.png" alt="account"></NuxtLink>
             </button>
-            <button class="rounded-full bg-yellow-300">
-                <img src="" alt="Workshops">
+            <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                <NuxtLink to="/makerspacejb/booking"><img src="/makerspaceJB/booking.png" alt="Booking"></NuxtLink>
             </button>
-            <button class="rounded-full bg-yellow-300">
-                <img src="" alt="Projects">
+            <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                <NuxtLink to="/makerspacejb/workshops"><img src="/makerspaceJB/workshops.png" alt="Workshops"></NuxtLink>
             </button>
-            <button class="rounded-full bg-yellow-300">
-                <img src="" alt="Booking">
-            </button>
-            <button class="rounded-full bg-yellow-300">
-                <img src="" alt="Cart">
+            <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                <NuxtLink to="/makerspacejb/projects"><img src="/makerspaceJB/projects.png" alt="Projects"></NuxtLink>
             </button>
         </div>
     </div>
--- pages/MakerspaceJB/home.vue
+++ pages/MakerspaceJB/home.vue
@@ -8,6 +8,24 @@
                 <img src="/makerspacejb//makerspacejb-banner.png" alt="">
             </div>
         </div>
+        <div class="bg-white bg-opacity-20 rounded-full border border-gray-500 text-white p-5 absolute right-0 top-0 m-5">
+            <h3 class="text-xl font-extrabold font-roboto text-gray-500 py-2">Quick<br>Links</h3>
+            <div class="flex flex-col">
+                <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                    <NuxtLink to="/makerspacejb/cart"><img src="/makerspaceJB/cart.png" alt="account"></NuxtLink>
+                </button>
+                <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                    <NuxtLink to="/makerspacejb/booking"><img src="/makerspaceJB/booking.png" alt="Booking"></NuxtLink>
+                </button>
+                <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                    <NuxtLink to="/makerspacejb/workshops"><img src="/makerspaceJB/workshops.png" alt="Workshops"></NuxtLink>
+                </button>
+                <button class="rounded-full bg-blue-100 border-2 border-black h-10 w-10 p-2 my-2 mx-1">
+                    <NuxtLink to="/makerspacejb/projects"><img src="/makerspaceJB/projects.png" alt="Projects"></NuxtLink>
+                </button>
+            </div>
+        </div>
+<!--
         <div>
             <QuickStart />
         </div>
@@ -28,6 +46,7 @@
                 </button>
             </div>
         </div>
+-->
         <div id="body" class="m-24">
             <div id="opening-message" class="py-5">
                 <h3 class="text-oswald text-3xl py-2">Visiting the space during COVID-19</h3>
