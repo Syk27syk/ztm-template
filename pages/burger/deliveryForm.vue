d69b27f5c12e8f258cf7de36742f92bd510ef780
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-19 07:59:25 +0800

    burger delivery form

---
+++ pages/burger/deliveryForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header>
+            <div id="account navigation">
+                <img src="" alt="" />
+                <v-toggle>
+                    English
+                    Chinese
+                    Malay
+                </v-toggle>
+                <div id="account dropdown menu">
+                    <img src="" alt="" />
+                    <h5> {{ username }} </h5>
+                    <button> v </button>
+                </div>
+            </div>
+        </header>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+        <footer>
+
+        <footer>
+    </div>
+</template>
