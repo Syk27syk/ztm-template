f32731fb23d2a3820b8da52baab57260a90ddf02
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-18 21:16:40 +0800

    edits day 16 assignment

--- pages/burger/cart.vue
+++ pages/burger/cart.vue
@@ -31,26 +31,31 @@
                     </tr>
                 </tbody>
                 <tfoot>
-                    <tr class="font-oswald font-bold text-2xl bg-gray-100 uppercase">
+                    <tr class="font-oswald font-bold text-2xl bg-gray-100 border-t border-b uppercase">
                         <td class="py-10" colspan="3">Total</td>
                         <td class="text-right">RM {{ total }}</td>
-                        <td class="invisible">text</td>
+                        <td class="text-transparent">text</td>
                     </tr>
                     <tr>
                       <td colspan="3">
-                        <input id="email" type="email" placeholder="Please enter your email" class="border border-gray-300 text-xl mt-5 py-3 px-2" />
+                        <input
+                          id="email"
+                          type="email"
+                          placeholder="Please enter your email"
+                          class="w-full border border-gray-300 text-xl mt-5 py-3 px-2"
+                        />
                       </td>
                       <td colspan="2" class="text-right">
-                        <button class="font-oswald uppercase bg-red-500 text-white text-xl py-3 px-2 ml-5 mt-5" @click="submitOrder">
+                        <button
+                          class="font-oswald uppercase bg-red-500 text-white text-xl py-3 px-2 ml-5 mt-5"
+                          @click="submitOrder"
+                        >
                           Confirm My Order
                         </button>
                       </td>
                     </tr>
                 </tfoot>
             </table>
-            <button class="bg-red-500 text-white text-oswald text-2xl text-center px-8 py-3 rounded my-12 mx-60 grid grid-cols-1 item-center">
-                <NuxtLink to="/burger/checkout">Proceed to Checkout</NuxtLink>
-            </button>
         </div>
     </div>
 </template>
