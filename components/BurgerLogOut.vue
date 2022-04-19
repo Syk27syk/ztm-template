f3de2b469aab705247acbe78de8f645e98fab2c9
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-19 08:15:07 +0800

    burger log out

---
+++ components/BurgerLogOut.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3 class="text-3xl text-semibold m-5">Logging out?</h3>
+        <p class="py-10 text-lg m-5"> Thanks for stopping by. See you again soon!</p>
+        <div class="grid grid-cols-2">
+            <button class="px-20 py-8 m-5 text-semibold"> Cancel </button>
+            <button class="px-20 py-8 m-5 text-semibold"> Log out </button>
+        </div>
+        <button> x </button>
+    </div>
+</template>
+
+<!--
+I want the "logout" button to be automatically selected, and highlighted. I want it to respond to keyboard arrow buttons and enter button.
+-->
