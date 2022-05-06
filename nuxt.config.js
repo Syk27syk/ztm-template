d8e7229cb3e0930d02243e77c353f8229962c461
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-06 17:35:08 +0800

    nodemailer

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
