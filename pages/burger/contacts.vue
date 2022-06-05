9829557bb89a81e2ac5b2ae8c904087cf2698728
shiyunkhoo <shiyun.khoo@gmail.com>
2022-06-05 22:02:12 +0800

    edits

--- pages/burger/contacts.vue
+++ pages/burger/contacts.vue
@@ -2,12 +2,12 @@
     <div>
         <div class="bg-[url('/contacts-page.jpg')]">
             <BurgerNavBar />
+            <h1 class="font-oswald uppercase text-6xl text-center pt-24 pb-28 z-20 text-white">
+                Contacts
+            </h1>
         </div>
         <body>
-            <Items />
-            <Items />
-            <Items />
-            <Items />
+            <p> Text </p>
         </body>
         <div>
             <BurgerBottom />
--- pages/burger/locations.vue
+++ pages/burger/locations.vue
@@ -2,13 +2,16 @@
     <div>
         <div class="bg-black">
             <BurgerNavBar />
+            <h1 class="font-oswald uppercase text-6xl text-center pt-24 pb-28 z-20 text-white">
+                Locations
+            </h1>
         </div>
         <body class="p-10 bg-slate-100">
-            <p class="font-bold uppercase"> Headquarters </p>
+            <p class="font-bold"> Headquarters </p>
             <p class="font-bold"> America </p>
             <p class="font-bold"> Asia Pacific </p>
             <p class="font-bold"> Europe </p>
-            <p class="font-bold"> Australiasia </p>
+            <p class="font-bold"> Australasia </p>
             <p class="font-bold"> Middle East </p>
         </body>
         <div>
